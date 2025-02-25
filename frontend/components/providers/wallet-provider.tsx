670ccf097a2a6f69fb9792b10375fae2969230b4
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2025-02-25 11:14:15 -0700

    ad

--- frontend/components/providers/wallet-provider.tsx
+++ frontend/components/providers/wallet-provider.tsx
@@ -7,7 +7,12 @@ import { type State, WagmiProvider } from "wagmi";
 import { getConfig } from "@/lib/wagmi"; // your import path may vary
 import { useTheme } from "next-themes";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
-
+import { BitcoinWalletConnectors } from "@dynamic-labs/bitcoin";
+import { SolanaWalletConnectors } from "@dynamic-labs/solana";
+import {
+  DynamicContextProvider,
+  FilterChain,
+} from "@dynamic-labs/sdk-react-core";
 import { SolanaIcon, BitcoinIcon } from "@dynamic-labs/iconic";
 
 export function WalletProvider(props: {
@@ -21,7 +26,36 @@ export function WalletProvider(props: {
     <WagmiProvider config={config} initialState={props.initialState}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider modalSize="compact">
-          {props.children}
+          <DynamicContextProvider
+            settings={{
+              environmentId: process.env.NEXT_PUBLIC_DYNAMIC_BTC || "",
+              walletConnectors: [
+                BitcoinWalletConnectors,
+                SolanaWalletConnectors,
+              ],
+              overrides: {
+                views: [
+                  {
+                    type: "wallet-list",
+                    tabs: {
+                      items: [
+                        {
+                          label: { icon: <SolanaIcon /> },
+                          walletsFilter: FilterChain("SOL"),
+                        },
+                        {
+                          label: { icon: <BitcoinIcon /> },
+                          walletsFilter: FilterChain("BTC"),
+                        },
+                      ],
+                    },
+                  },
+                ],
+              },
+            }}
+          >
+            {props.children}
+          </DynamicContextProvider>
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
